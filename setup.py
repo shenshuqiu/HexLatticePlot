e68a2b27495122a3f547dd90579990f8964619bf
ssq <610124734@qq.com>
2024-06-11 17:08:25 +0800

    ✨ feat: Add Setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+# my_heat_simulation/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='HexLatticePlot',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+    ],
+    author='shenshuqiu',
+    description='A package for Hex Lattice Ploting',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/shenshuqiu/HexLatticePlot',
+    python_requires='>=3.10',
+)

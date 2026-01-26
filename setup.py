cd783eb217a29aadbf3916635073728e8f8c8331
shenshuqiu <610124734@qq.com>
2026-01-26 15:58:07 +0800

    chore(release): 1.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='HexLatticePlot',
-    version='1.1',
+    version='1.2.0',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
